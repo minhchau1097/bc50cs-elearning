571890b9cda7185b21da918ea1a3c08f4481eebb
BaoTheTran <ttbao2710@gmail.com>
2023-09-22 18:23:54 +0700

    loading code

--- src/page/HomeTemplate/HomePage/CourseList/index.tsx
+++ src/page/HomeTemplate/HomePage/CourseList/index.tsx
@@ -5,17 +5,21 @@ import CourseItem from './CourseItem';
 import { Course } from '../../../../type/type';
 import { RootState } from '../../../../store';
 import { AnimationOnScroll } from 'react-animation-on-scroll';
-import { Card, Pagination, Table } from 'antd';
+
 export default function CourseList() {
   const dispatch: any = useDispatch();
-  const dataList = useSelector((state: RootState) => state.courseListReducer.data);
+  const dataList :any = useSelector((state: RootState) => state.courseListReducer.data);
+  const newArray = dataList?.slice(0, 8);
+
 
   useEffect(() => {
     dispatch(actFetchListCourse());
   }, []);
 
+
+
   const renderCourseList = () => {
-    return dataList?.map((item: Course) => {
+    return newArray?.map((item: Course) => {
       return <CourseItem
         key={item.maKhoaHoc}
         item={item}
